d90aeecf45dfaa85d22e84fc6c6f943f2a307a23
Jussi Nousiainen <juno.athome@gmail.com>
2019-11-27 16:56:17 +0100

    Added name validation to the task names

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,97 +1,117 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-/*Styles*/
-// save project button
-$("#save-project").css({"background-color": "rgb(5, 199, 5, .5", "border-color": "var(--clr-green)"})
-$("#save-tasks").css({"background-color": "rgb(5, 199, 5, .5", "border-color": "var(--clr-green)"})
+    /*Styles*/
+    // save project button
+    $("#save-project").css({
+        "background-color": "rgb(5, 199, 5, .5",
+        "border-color": "var(--clr-green)"
+    });
+    $("#save-tasks").css({
+        "background-color": "rgb(5, 199, 5, .5",
+        "border-color": "var(--clr-green)"
+    });
 
 
-//index
-//query selectors
-const projectNameForm = document.getElementById('create-project-form')
-const projectName = document.getElementById('accept-project')
-const projectNameDisplay = document.getElementById('project-name-display')
-const projectCreateButton = document.getElementById('create-project-button')
-const projectResetButton = document.getElementById('projectReset')
-const projectNameInput = document.getElementById('projectName')
-const saveProjectButton = document.getElementById('save-project')
-const nameNotOkPrompt = document.getElementById('nameOk')
+    //index
+    //query selectors - this section is done using mainly JavaScript code, jQuery selectors  used for styling
+    const projectNameForm = document.getElementById('create-project-form');
+    const projectName = document.getElementById('accept-project');
+    const projectNameDisplay = document.getElementById('project-name-display');
+    const projectCreateButton = document.getElementById('create-project-button');
+    const projectResetButton = document.getElementById('projectReset');
+    const projectNameInput = document.getElementById('projectName');
+    const saveProjectButton = document.getElementById('save-project');
+    const nameNotOkPrompt = document.getElementById('nameOk');
 
-let savedProject = localStorage.getItem('project')
-var getProject;
- 
-//project creator gets the name input and creates a <p> element to display the name
-// for this constructor I used the code example from https://www.taniarascia.com/how-to-use-local-storage-with-javascript/
-const projectCreator = (text) => {
-    const pName = document.createElement('p')
-    pName.textContent = text
-    projectNameDisplay.appendChild(pName)
-}
- 
-// clicking 'enter' submits the name value and the name form is hidden from view
+    let savedProject = localStorage.getItem('project');
+    let getProject;
 
-projectCreateButton.addEventListener('click', function (e) {
-    e.preventDefault()
+    //project creator gets the name input and creates a <p> element to display the name
+    // for this constructor I used the code example from https://www.taniarascia.com/how-to-use-local-storage-with-javascript/
+    const projectCreator = (text) => {
+        const pName = document.createElement('p');
+        pName.textContent = text
+        projectNameDisplay.appendChild(pName);
+    };
 
-    projectCreator(projectNameInput.value)
-    if(projectNameInput.value === null || projectNameInput.value == "") {
-       noNamePrompt();
-    } else {
-    if(projectNameForm.style.display === "none") {
-        projectNameForm.style.display = "flex";
-        
-    } else {
-        projectNameForm.style.display = "none";
-        projectName.style.display = "flex";
-    }
-    addProjectButtonListener ();
-}
-})
+    // clicking 'enter' submits the name value and the name form is hidden from view
 
- 
-// the name value is displayed with a cancel button. On click,,
-//the <p> element displaying the name removed, name form displayed and the name form field is emptied.
-projectResetButton.addEventListener('click', function(e) {
-    e.preventDefault()
-    
-    while (projectNameDisplay.firstChild) {
-        projectNameDisplay.removeChild(projectNameDisplay.firstChild)
-      }
-    if(projectName.style.display === "flex") {
-        projectName.style.display = "none";
-        projectNameForm.style.display = "flex";
-    }
+    projectCreateButton.addEventListener('click', (e) => {
+        e.preventDefault();
 
-    saveProjectButton.removeEventListener('click', saveProjectToLocal);
-    $("#toTasks").removeAttr("href")
-    $("#save-project").css({"background-color": "rgb(5, 199, 5, .5", "border-color": "var(--clr-green)"})
+        projectCreator(projectNameInput.value);
+        if (projectNameInput.value === null || projectNameInput.value == "") {
+            noNamePrompt();
+        } else {
+            if (projectNameForm.style.display === "none") {
+                projectNameForm.style.display = "flex";
 
-    projectNameInput.value = ''
-})
- 
-function addProjectButtonListener () {
-    
-    saveProjectButton.addEventListener('click', saveProjectToLocal);
-    $("#toTasks").attr("href", "tasks.html", "target", "_blank")
-    $("#save-project").css({"background-color": "var(--clr-green)", "border-color": "var(--clr-green)"})
+            } else {
+                projectNameForm.style.display = "none";
+                projectName.style.display = "flex";
+            }
+            addProjectButtonListener();
+        }
+    });
 
-}
 
-function noNamePrompt () {
-    $("#noNamePrompt").css({"display": "block"})
-    $(".fa-hourglass").css({"color": "rgba(94, 1, 94, 0.3)"})
-    nameNotOkPrompt.addEventListener('click', function() {
-        $("#noNamePrompt").css({"display": "none"})
-        nameNotOkPrompt.addEventListener('click', (this))
-        $(".fa-hourglass").css({"color": "rgba(94, 1, 94, 0.6)"})
-    })
-    
-}
+    // the name value is displayed with a cancel button. On click,,
+    //the <p> element displaying the name removed, name form displayed and the name form field is emptied.
+    projectResetButton.addEventListener('click', (e) => {
+        e.preventDefault();
 
-function saveProjectToLocal() {
-    savedProject = projectNameInput.value;
-    localStorage.setItem('project', savedProject);
-}
+        while (projectNameDisplay.firstChild) {
+            projectNameDisplay.removeChild(projectNameDisplay.firstChild)
+        }
+        if (projectName.style.display === "flex") {
+            projectName.style.display = "none";
+            projectNameForm.style.display = "flex";
+        };
+
+        saveProjectButton.removeEventListener('click', saveProjectToLocal);
+        $("#toTasks").removeAttr("href");
+        $("#save-project").css({
+            "background-color": "rgb(5, 199, 5, .5",
+            "border-color": "var(--clr-green)"
+        });
+
+        projectNameInput.value = ''
+    });
+
+    function addProjectButtonListener() {
+
+        saveProjectButton.addEventListener('click', saveProjectToLocal);
+        $("#toTasks").attr("href", "tasks.html", "target", "_blank");
+        $("#save-project").css({
+            "background-color": "var(--clr-green)",
+            "border-color": "var(--clr-green)"
+        });
+
+    };
+
+    function noNamePrompt() {
+        $("#noNamePrompt").css({
+            "display": "block"
+        });
+        $(".fa-hourglass").css({
+            "color": "rgba(94, 1, 94, 0.3)"
+        });
+        nameNotOkPrompt.addEventListener('click', function () {
+            $("#noNamePrompt").css({
+                "display": "none"
+            });
+            nameNotOkPrompt.addEventListener('click', (this));
+            $(".fa-hourglass").css({
+                "color": "rgba(94, 1, 94, 0.6)"
+            });
+        });
+
+    };
+
+    function saveProjectToLocal() {
+        savedProject = projectNameInput.value;
+        localStorage.setItem('project', savedProject);
+    };
 
 
 
--- assets/js/tasksScript.js
+++ assets/js/tasksScript.js
@@ -1,12 +1,98 @@
 $(document).ready(function setProject() {
 
-/*Styles*/
-// save project button
-$("#save-project").css({"background-color": "rgb(5, 199, 5, .5", "border-color": "var(--clr-green)"})
-$("#save-tasks").css({"background-color": "rgb(5, 199, 5, .5", "border-color": "var(--clr-green)"})
+    /*Styles*/
+    // save project button
+    $("#save-project").css({
+        "background-color": "rgb(5, 199, 5, .5",
+        "border-color": "var(--clr-green)"
+    })
+    $("#save-tasks").css({
+        "background-color": "rgb(5, 199, 5, .5",
+        "border-color": "var(--clr-green)"
+    })
 
-    var getProject = localStorage.getItem('project');
-    $(".project-title").text(getProject)
+    // setting the project name title
+    let getProject = localStorage.getItem('project');
+    $(".project-title").text(getProject);
+
+    /*let task 
+
+    [
+        {
+            "id": id,
+            "name": name,
+            "startTime": null,
+            "endTime": null,
+            "breaks": 0
+        }
+    ]*/
+
+    //let taskList = localStorage.setItem()
+
+
+    const $newTaskButton = $('#newTaskButton');
+    const $newTaskForm = $('#newTaskForm');
+    const $nameInput = $('input:text');
+
+    const $emptyName = $('#emptyNamePrompt');
+    const $taskItems = $('#taskItems')
+
+    $newTaskForm.show();
+    $newTaskButton.hide();
+
+    $newTaskForm.on('submit', (e) => {
+        e.preventDefault();
+        var newName = $nameInput.val();
+        newName = jQuery.trim(newName);
+            console.log(newName)
+            console.log(newName.length) /* add input validation!!*/
+        if (newName === null || newName == "" || newName.length === 0) {
+            emptyNamePrompt();
+            console.log(typeof(newName));
+        } else {
+            newName = newName.charAt(0).toUpperCase() + newName.slice(1);
+            $taskItems.after(
+                '<form>' +
+                '<li class="task">' + newName +
+                '<button type="submit" class="btn btn-default remove task-button" id="removeTask" data-toggle="tooltip" title="Remove task"><i class="fa fa-minus-circle task-icon"></i></button>' +
+                '</li>' + '</form>');
+
+            $newTaskForm.hide();
+            $newTaskButton.show();
+            $nameInput.val('');
+        }
+    });
+
+    $('#showTasks').on('click', () => {
+        $newTaskButton.hide();
+        $newTaskForm.show();
+        $('.newTasks').scrollTop($('.newTasks')[0].scrollHeight);
+    });
+
+
+    /* Removing Tasks */
+
+    $("#removeTask").on('submit', (e) => {
+        $(this).remove(".task");
+    });
+
+
+    function emptyNamePrompt() {
+        $emptyName.css({
+            "display": "block"
+        });
+
+        $emptyName.on('click', function () {
+            $emptyName.css({
+                "display": "none"
+            });
+            $nameInput.val('')
+        });
+    };
+
+
+
+
 
 
 });
